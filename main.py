03b6efedd1d2ab08615f272aa86231b00628d325
VladimirSteshin <warheim24@gmail.com>
2022-04-27 22:01:50 +0300

    XML done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
+import xml.etree.ElementTree as ET
 import collections
 from pprint import pprint
 
 
-def counting_words(path, words_len, top):
+def counting_words_json(path, words_len, top):
     with open(path, encoding='UTF-8') as file:
         work = json.load(file)
         words_list = []
@@ -14,4 +15,19 @@ def counting_words(path, words_len, top):
         pprint(top_counter.most_common(top))
 
 
-counting_words('newsafr.json', 6, 10)
+def counting_words_xml(path, words_len, top):
+    parser = ET.XMLParser(encoding='UTF-8')
+    work = ET.parse(path, parser)
+    root = work.getroot()
+    items = root.findall('channel/item')
+    news_list = []
+    for news in items:
+        text = news.find('description').text.split()
+        merge = [word for word in text if len(word) > words_len]
+        news_list.extend(merge)
+    counter = collections.Counter(news_list)
+    pprint(counter.most_common(top))
+
+
+counting_words_json('newsafr.json', 6, 10)
+counting_words_xml('newsafr.xml', 6, 10)
